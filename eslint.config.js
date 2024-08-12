d9807b694db40f11ee2ce97b0e1411defc454ea1
Dessel Bane <desselbane999+github@gmail.com>
2024-08-12 11:41:19 +0200

    setup auto linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
-// @ts-check
-
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    eslintPluginPrettierRecommended
-);
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+)
